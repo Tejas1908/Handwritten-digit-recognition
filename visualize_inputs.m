803906204a1df8799f4d3296422a1af52d789fe0
Tejas1908 <36530921+Tejas1908@users.noreply.github.com>
2018-07-23 14:31:30 +0530

    Delete visualize_inputs.m

--- visualize_inputs.m
+++
@@ -1,9 +0,0 @@
-figure                                          % initialize figure
-colormap(gray)                                  % set to grayscale
-for i = 1:36                                    % preview first 36 samples
-    subplot(6,6,i)                              % plot them in 6 x 6 grid
-    digit = reshape(trainX(i, :), [28,28]);
-    digit=digit';                               % row = 28 x 28 image
-    imagesc(digit)                              % show the image
-    title(num2str(trainY(i)))                   % show the label
-end
